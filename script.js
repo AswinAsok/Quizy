f5ab179a3bf53b9ac0c5702207c01ebca25dbf7f
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-10 18:28:50 +0530

    Instructions page Javascript added

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const nextbtn = document.querySelector(".nextbtn");
-const rulescontainer = document.querySelector(".rules-container")
+const rulescontainer = document.querySelector(".rules-container");
 
-const hide = () => {
-  nextbtn.parentElement.parentElement.classList.add("hide")
-  rulescontainer.classList.remove("hide")
+const hideheader = () => {
+  nextbtn.parentElement.parentElement.classList.add("hide");
+  rulescontainer.classList.remove("hide");
 };
 
-nextbtn.addEventListener("click", hide);
+nextbtn.addEventListener("click", hideheader);
+
+const startbtn = document.querySelector(".startbtn");
+const questionscontainer = document.querySelector(".questions-container");
+
+const hiderules = () => {
+  startbtn.parentElement.parentElement.parentElement.classList.add("hide");
+  questionscontainer.classList.remove("hide");
+  console.log(startbtn.parentElement.parentElement.parentElement)
+};
+
+startbtn.addEventListener("click", hiderules)
