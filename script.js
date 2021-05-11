5f9fb24eed2496d06ca09d44843c445d94ec2cd1
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-11 07:01:56 +0530

    Options are added

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ const api_url = `https://quizapi.io/api/v1/questions?apiKey=VAa9sQhkjH5GUnCLeqG3
 const hiderules = () => {
   startbtn.parentElement.parentElement.parentElement.classList.add("hide");
   questionscontainer.classList.remove("hide");
-  getdata(api_url);
 };
-
+getdata(api_url);
 startbtn.addEventListener("click", hiderules);
 
 // -------- API Call --------
@@ -25,15 +24,46 @@ async function getdata(url) {
   const response = await fetch(url);
 
   var data = await response.json();
-
+  console.log(data);
   addquestion(data);
 }
 
 // --------- Adding Question ------
 var question_no = 1;
+
 const main_question = document.querySelector(".main-question");
+const options = document.querySelector(".options");
 const addquestion = (data) => {
   if (data) {
-    main_question.innerHTML = data[question_no].question;
+    main_question.innerHTML =
+      question_no + "). " + data[question_no - 1].question;
+
+    if (data[question_no - 1].answers.answer_a !== null) {
+      const opbtn1 = document.createElement("button");
+      opbtn1.innerText = data[question_no - 1].answers.answer_a;
+      opbtn1.classList.add("option-1");
+      options.appendChild(opbtn1);
+    }
+
+    if (data[question_no - 1].answers.answer_b !== null) {
+      const opbtn2 = document.createElement("button");
+      opbtn2.innerText = data[question_no - 1].answers.answer_b;
+      opbtn2.classList.add("option-2");
+      options.appendChild(opbtn2);
+    }
+
+    if (data[question_no - 1].answers.answer_c !== null) {
+      const opbtn3 = document.createElement("button");
+      opbtn3.innerText = data[question_no - 1].answers.answer_c;
+      opbtn3.classList.add("option-3");
+      options.appendChild(opbtn3);
+    }
+
+    if (data[question_no - 1].answers.answer_d !== null) {
+      const opbtn4 = document.createElement("button");
+      opbtn4.innerText = data[question_no - 1].answers.answer_d;
+      opbtn4.classList.add("option-4");
+      options.appendChild(opbtn4);
+    }
   }
 };
