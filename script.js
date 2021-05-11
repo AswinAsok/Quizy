2c756f1632bc6e3e0f4639ac5b37c14a44dfda5c
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-11 10:35:31 +0530

    Its working

--- script.js
+++ script.js
@@ -19,58 +19,73 @@ const hiderules = () => {
 getdata(api_url);
 startbtn.addEventListener("click", hiderules);
 
+var question_number = 0;
+
 // -------- API Call --------
 async function getdata(url) {
   const response = await fetch(url);
-
+  question_number += 1;
   var data = await response.json();
   console.log(data);
-  addquestion(data);
+  addquestion(data, question_number);
 }
 
 // --------- Adding Question ------
-var question_no = 1;
 
-const main_question = document.querySelector(".main-question");
-const options = document.querySelector(".options");
+const addquestion = (data, index) => {
+  const questions_container = document.querySelector(".questions-container");
 
+  const question_box = document.createElement("div");
+  question_box.classList.add("question-box");
+  questions_container.appendChild(question_box);
 
+  const questions = document.createElement("div");
+  questions.classList.add("questions");
+  question_box.appendChild(questions);
 
-
-
-const addquestion = (data) => {
   if (data) {
-    main_question.innerHTML =
-      question_no + "). " + data[question_no - 1].question;
+    const main_question = document.createElement("p");
+    main_question.classList.add("main-question");
+    questions.appendChild(main_question);
 
-    if (data[question_no - 1].answers.answer_a !== null) {
+    const options = document.createElement("div");
+    options.classList.add("options");
+    questions.appendChild(options);
+
+    main_question.innerHTML = index + "). " + data[index - 1].question;
+
+    if (data[index - 1].answers.answer_a !== null) {
       const opbtn1 = document.createElement("button");
-      opbtn1.innerText = data[question_no - 1].answers.answer_a;
+      opbtn1.innerText = data[index - 1].answers.answer_a;
       opbtn1.classList.add("option-1");
       options.appendChild(opbtn1);
     }
 
-    if (data[question_no - 1].answers.answer_b !== null) {
+    if (data[index - 1].answers.answer_b !== null) {
       const opbtn2 = document.createElement("button");
-      opbtn2.innerText = data[question_no - 1].answers.answer_b;
+      opbtn2.innerText = data[index - 1].answers.answer_b;
       opbtn2.classList.add("option-2");
       options.appendChild(opbtn2);
     }
 
-    if (data[question_no - 1].answers.answer_c !== null) {
+    if (data[index - 1].answers.answer_c !== null) {
       const opbtn3 = document.createElement("button");
-      opbtn3.innerText = data[question_no - 1].answers.answer_c;
+      opbtn3.innerText = data[index - 1].answers.answer_c;
       opbtn3.classList.add("option-3");
       options.appendChild(opbtn3);
     }
 
-    if (data[question_no - 1].answers.answer_d !== null) {
+    if (data[index - 1].answers.answer_d !== null) {
       const opbtn4 = document.createElement("button");
-      opbtn4.innerText = data[question_no - 1].answers.answer_d;
+      opbtn4.innerText = data[index - 1].answers.answer_d;
       opbtn4.classList.add("option-4");
       options.appendChild(opbtn4);
     }
   }
 };
 
-
+const nextquestion = () => {
+  getdata(api_url);
+  const questions_container = document.querySelector(".questions-container");
+  questions_container.innerHTML = "";
+};
