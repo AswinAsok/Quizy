37c5a33eff549998a88ef521a42e05c45ae1066f
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-11 05:57:52 +0530

    Api call success

--- script.js
+++ script.js
@@ -10,11 +10,22 @@ nextbtn.addEventListener("click", hideheader);
 
 const startbtn = document.querySelector(".startbtn");
 const questionscontainer = document.querySelector(".questions-container");
+const api_url = `https://quizapi.io/api/v1/questions?apiKey=VAa9sQhkjH5GUnCLeqG3xTo4rqUEz7Z0gFhi0ZaX&limit=10&tags=javascript`;
 
 const hiderules = () => {
   startbtn.parentElement.parentElement.parentElement.classList.add("hide");
   questionscontainer.classList.remove("hide");
-  console.log(startbtn.parentElement.parentElement.parentElement)
+  console.log(questionscontainer)
+  getdata(api_url);
 };
 
-startbtn.addEventListener("click", hiderules)
+startbtn.addEventListener("click", hiderules);
+
+// -------- API Call --------
+
+async function getdata(url) {
+  const response = await fetch(url);
+
+  var data = await response.json();
+  console.log(data);
+}
